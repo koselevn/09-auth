af328a963223b3b2814488b2fd69a54a4910bbc4
koselevn <koselevn52@gmail.com>
2025-11-10 20:38:10 +0100

    save commit 5

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import css from "./NoteForm.module.css";
-import { createNote } from "../../lib/api";
+import { createNote } from "../../lib/api/clientApi";
 import { useRouter } from 'next/navigation'
 import { useDraft } from "@/lib/store/noteStore";
 
