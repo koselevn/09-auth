736c65c26b6375da15022000b47afcb7f34666c9
koselevn <koselevn52@gmail.com>
2025-12-10 18:26:33 +0100

    d2-v1

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,15 +1,6 @@
-import axios, { AxiosError } from 'axios';
-
-export type ApiError = AxiosError<{ error: string }>
-
-const myKey: string = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN!;
-
+import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://notehub-api.goit.study',
   withCredentials: true,
-  headers: {
-    Authorization: `Bearer ${myKey}`,
-  }
 });
-
